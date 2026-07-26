6d7202f52a09f021886c5e7f7f2afa08cc5dffe4
Hajnalka Bőr <hajnalka.bor@abzinnovation.com>
2026-07-26 11:40:06 +0200

    study notes

--- file_io.py
+++ file_io.py
@@ -1,4 +1,4 @@
-import json
+import json                                                                         #study_note_03
 import os
 from platformdirs import user_data_dir
 import tempfile
@@ -15,15 +15,19 @@ USER_JSON = os.path.join(USER_DIR, "user_mind_map.json")
 
 
 def save_elements(elements):
-    # os.makedirs(USER_DIR, exist_ok=True)
-    # with open(USER_JSON, "w", encoding="utf-8") as f:
-    #     json.dump(elements, f, indent=4, ensure_ascii=False)    
+    # (Study NOTE: Atomic writing: https://sahmanish20.medium.com/better-file-writing-in-python-embrace-atomic-updates-593843bfab4f )
+    
     os.makedirs(USER_DIR, exist_ok=True)
+    
+    # Create temporary file
     fd, tmp_path = tempfile.mkstemp(dir=USER_DIR, suffix=".tmp")
+    
+    # Try write but prepare for an exeption
+    # Study NOTE: https://zetcode.com/python/os-replace/
     try:
         with os.fdopen(fd, "w", encoding="utf-8") as f:
             json.dump(elements, f, indent=4, ensure_ascii=False)
-        os.replace(tmp_path, USER_JSON)  # atomi csere, csak siker esetén
+        os.replace(tmp_path, USER_JSON)
     except Exception:
         os.remove(tmp_path)
         raise
@@ -38,4 +42,11 @@ def load_elements():
         return default_elements
     with open(USER_JSON, "r", encoding="utf-8") as f:
         return json.load(f)
-    
+
+
+
+    # Study NOTE: https://python.plainenglish.io/simple-safe-atomic-writes-in-python3-44b98830a013
+
+    # Study NOTE: https://www.w3schools.com/python/ref_os_makedirs.asp
+   
+    # Study NOTE: https://docs.python.org/3/library/tempfile.html#:~:text=tempfile.-,mkstemp,-(suffix%3D
